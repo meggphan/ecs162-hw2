87aadf5d3010144bb55d95a9991b7159649f62be
meggphan <168615838+meggphan@users.noreply.github.com>
2025-05-01 15:02:16 -0700

    API key unit test

--- frontend/src/App.test.ts
+++ frontend/src/App.test.ts
@@ -1,7 +1,13 @@
-import { test } from 'vitest';
+import { test, describe, it, expect, vi } from 'vitest';
 import { render } from '@testing-library/svelte';
 import App from './App.svelte';
 
-test('App', async () => {
-  render(App);
+describe('get apikey', () => {
+  it('should return an object with apiKey property', async () => {
+    const response = await fetch('http://localhost:8000/api/key');
+    expect(response.status).toBe(200);
+    const data = await response.json();
+    expect(data).toHaveProperty('apiKey');
+    expect(data.apiKey.length).toBeGreaterThan(0);
+  });
 });
